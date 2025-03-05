108edc9e3f80e0fba6e9760adb468eeb803d1b84
Liam <lima.mail725@gmail.com>
2025-03-06 01:21:39 +1000

    Broke unscrambler out into separate file.

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,19 @@
-use std::{
-    collections::{HashSet, VecDeque},
-    fs::File,
-    io::{BufRead, BufReader, Error},
-    path::Path,
-};
+use std::path::Path;
 
-fn load_dictionary(path: &Path) -> Result<HashSet<String>, Error> {
-    let mut dictionary = HashSet::new();
-    let file = match File::open(path) {
-        Ok(file) => file,
-        Err(error) => return Err(error),
-    };
-    let reader = BufReader::new(file);
+use unscrambler::Unscrambler;
 
-    for line in reader.lines() {
-        if line.is_err() {
-            continue;
-        }
-        dictionary.insert(line.unwrap().to_lowercase());
-    }
-
-    Ok(dictionary)
-}
-
-struct SearchData {
-    pub building: String,
-    pub letters: String,
-}
+mod unscrambler;
 
 fn main() {
-    let dictionary = match load_dictionary(Path::new("dict.txt")) {
-        Ok(dictionary) => dictionary,
+    let mut unscrambler = Unscrambler::new();
+    match unscrambler.load_dictionary(Path::new("dict.txt")) {
+        Ok(_) => {}
         Err(error) => panic!("Failed to load dictionary: {}", error),
-    };
-
-    let start_data = SearchData {
-        building: String::new(),
-        letters: String::from("testing"),
-    };
-
-    let mut search_queue = VecDeque::new();
-    search_queue.push_back(start_data);
-
-    let mut found_words = Vec::new();
-
-    while let Some(search_data) = search_queue.pop_front() {
-        let SearchData { building, letters } = search_data;
-
-        if dictionary.contains(&building) {
-            found_words.push(building.clone());
-        }
-
-        for i in 0..letters.len() {
-            let ch = letters.chars().nth(i).unwrap();
-
-            let mut new_building = building.clone();
-            new_building.push(ch);
-
-            let mut new_letters = letters.clone();
-            new_letters.remove(i);
-
-            let new_data = SearchData {
-                building: new_building,
-                letters: new_letters,
-            };
-
-            search_queue.push_back(new_data);
-        }
     }
 
+    let found_words = unscrambler.unscramble(String::from("letters"));
+
+    println!("{} words found", found_words.len());
     for word in found_words {
         println!("{}", word);
     }
---
+++ src/unscrambler.rs
@@ -0,0 +1,89 @@
+use std::{
+    collections::{HashSet, VecDeque},
+    fs::File,
+    io::{BufRead, BufReader, Error},
+    path::Path,
+};
+
+pub struct Unscrambler {
+    dictionary: HashSet<String>,
+}
+
+impl Unscrambler {
+    /// Creates a new [`Unscrambler`].
+    pub fn new() -> Self {
+        Self {
+            dictionary: HashSet::new(),
+        }
+    }
+
+    /// Loads the dictionary into the unscrambler.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if the file does not exist.
+    pub fn load_dictionary(&mut self, path: &Path) -> Result<(), Error> {
+        let file = match File::open(path) {
+            Ok(file) => file,
+            Err(error) => return Err(error),
+        };
+        let reader = BufReader::new(file);
+
+        for line in reader.lines() {
+            if line.is_err() {
+                continue;
+            }
+            self.dictionary.insert(line.unwrap().to_lowercase());
+        }
+
+        Ok(())
+    }
+
+    /// Generates a [`HashSet`] of words found.
+    pub fn unscramble(&self, letters: String) -> HashSet<String> {
+        let start_data = SearchData {
+            building: String::new(),
+            letters,
+        };
+
+        let mut search_queue = VecDeque::new();
+        search_queue.push_back(start_data);
+
+        let mut found_words = HashSet::new();
+
+        while let Some(search_data) = search_queue.pop_front() {
+            let SearchData { building, letters } = search_data;
+
+            if self.dictionary.contains(&building) {
+                found_words.insert(building.clone());
+            }
+
+            for i in 0..letters.len() {
+                let ch = match letters.chars().nth(i) {
+                    Some(character) => character,
+                    None => continue,
+                };
+
+                let mut new_building = building.clone();
+                new_building.push(ch);
+
+                let mut new_letters = letters.clone();
+                new_letters.remove(i);
+
+                let new_data = SearchData {
+                    building: new_building,
+                    letters: new_letters,
+                };
+
+                search_queue.push_back(new_data);
+            }
+        }
+
+        found_words
+    }
+}
+
+struct SearchData {
+    pub building: String,
+    pub letters: String,
+}
